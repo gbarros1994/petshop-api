3e6ac55c854de77a12fc8f42a032b042079998a9
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2021-01-14 02:18:12 -0300

    rotas / versionamento de rotas

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -1,19 +1,24 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Delete, Get, Post, Put } from "@nestjs/common";
 
-@Controller()
+// localhost:3000/customers
+@Controller('customers')
 export class CustomerController {
+    @Get()
     get() {
         return 'Obter';
     }
 
+    @Post()
     post() {
         return 'Criar';
     }
 
+    @Put()
     put() {
         return 'Atualizar';
     }
 
+    @Delete()
     delete() {
         return 'Remover';
     }
