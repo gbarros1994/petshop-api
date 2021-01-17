63f2c7ca7b8e4a7da4853811e103257a8a25c8ee
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2021-01-17 19:43:36 -0300

    modelagem

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -1,34 +1,32 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { Customer } from "../models/customer.model";
+import { Result } from "../models/result.model";
 
 // localhost:3000/customers
 @Controller('v1/customers')
 export class CustomerController {
     @Get()
     get() {
-        return 'Obter';
+        return new Result(null, true, [], null);
     }
 
     @Get(':document')
     getById(@Param('document') document) {
-        return 'Obter ' + document;
+        return new Result(null, true, {}, null);
     }
 
     @Post()
     post(@Body() body: Customer) {
-        return body.name;
+        return new Result('Cliente criado com sucesso!', true, body, null);
     }
 
     @Put(':document')
     put(@Param('document')  document, @Body() body) {
-        return {
-            customer: document,
-            data: body
-        };
+        return new Result('Cliente alterado com sucesso!', true, body, null);
     }
 
     @Delete(':document')
     delete(@Param(':document') document) {
-        return 'Remover';
+        return new Result('Cliente removido com sucesso!', true, null, null);
     }
 }
--- src/backoffice/models/customer.model.ts
+++ src/backoffice/models/customer.model.ts
@@ -1,9 +1,16 @@
+import { Address } from 'cluster';
+import { CreditCard } from './credit-card.model';
+import { Pet } from './pet.model';
 
 export class Customer {
     constructor(
         public name: string,
         public document: string,
         public email: string,
+        public pets: Pet[],
+        public billingAddress: Address,
+        public shippingAddress: Address,
+        public creditCard: CreditCard,
         public password: string,
         public active: boolean,
     ) { 
