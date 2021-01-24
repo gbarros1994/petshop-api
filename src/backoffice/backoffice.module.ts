ae84493cbc54e53aa9b079fa8603542fd22866b8
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2021-01-23 23:30:57 -0300

    mongoose

--- src/backoffice/backoffice.module.ts
+++ src/backoffice/backoffice.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CustomerController } from './controllers/customer.controller';
+import { CustomerSchema } from './schemas/customer.schema';
+import { UserSchema } from './schemas/user.schema';
 
 @Module({
+    imports: [MongooseModule.forFeature([
+        {
+            name: 'Customer',
+            schema: CustomerSchema,
+        },
+        {
+            name: 'User',
+            schema: UserSchema,
+        },
+    ])],
     controllers: [CustomerController]
 })
 export class BackofficeModule {}
